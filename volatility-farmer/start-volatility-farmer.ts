e288dc773e45c51892045602af181b9f78463a0e
Michael <michael.spengler@sap.com>
2021-10-04 14:45:39 +0200

    volatility farming

---
+++ volatility-farmer/start-volatility-farmer.ts
@@ -0,0 +1,23 @@
+
+import { VolatilityFarmer } from "https://deno.land/x/exchange_connectors@latest/src/volatility-farming/volatility-farmer/volatility-farmer.ts"
+
+const apiKey = Deno.args[0]
+const apiSecret = Deno.args[1]
+
+const dbUser = Deno.args[2]
+const dbPW = Deno.args[3]
+
+const minimumReserve = 70
+const intervalLengthInSeconds = 4
+
+let assetManager: VolatilityFarmer
+
+if (dbUser !== undefined && dbPW !== undefined) {
+    const dbConnectionURL = `mongodb://${dbUser}:${dbPW}@localhost:27017`
+    assetManager = new VolatilityFarmer(apiKey, apiSecret, minimumReserve, undefined, dbConnectionURL)
+} else {
+    assetManager = new VolatilityFarmer(apiKey, apiSecret, minimumReserve)
+}
+
+assetManager.optimizeInvestments(intervalLengthInSeconds)
+
