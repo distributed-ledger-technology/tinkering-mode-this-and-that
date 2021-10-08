da3b39775cbc5464dc7f1e1aef972312846f4b43
Michael <michael.spengler@sap.com>
2021-10-08 22:53:42 +0200

    update dep ref

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/exchange_connectors@v1.4.4/mod-bybit.ts"
-export * from "https://deno.land/x/exchange_connectors@v1.4.4/mod-volatility-farmer.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.5.0/mod-bybit.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.5.0/mod-volatility-farmer.ts"
