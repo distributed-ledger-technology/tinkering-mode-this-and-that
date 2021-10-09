ba7537e8dc27e60f67e519b9a2ccc42da9cb03b5
Michael <michael.spengler@sap.com>
2021-10-10 00:55:09 +0200

    deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/exchange_connectors@v1.6.4/mod-bybit.ts"
-export * from "https://deno.land/x/exchange_connectors@v1.6.4/mod-volatility-farmer.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.7.0/mod-bybit.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.7.0/mod-volatility-farmer.ts"
 export * from "https://deno.land/x/injector@v1.3.0/mod.ts"
