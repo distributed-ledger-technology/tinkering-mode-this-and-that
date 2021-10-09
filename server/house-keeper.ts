fe6c877f00d5d1faa53959e93df97105f5c82012
Michael <michael.spengler@sap.com>
2021-10-09 22:13:41 +0200

    house keeper + deps

--- server/house-keeper.ts
+++ server/house-keeper.ts
@@ -12,12 +12,17 @@ export class HouseKeeper {
 
         setInterval(async () => {
 
-            const accounts = await this.mongoService.readAccountInfo()
+            try {
 
-            for (const account of accounts) {
-                console.log(account.apiKey)
-                await MongoService.deleteOldLogEntries(this.mongoService, account.apiKey)
-                await MongoService.deleteOldDealEntries(this.mongoService, account.apiKey)
+                const accounts = await this.mongoService.readAccountInfo()
+
+                for (const account of accounts) {
+                    console.log(account.apiKey)
+                    await MongoService.deleteOldLogEntries(this.mongoService, account.apiKey)
+                    await MongoService.deleteOldDealEntries(this.mongoService, account.apiKey)
+                }
+            } catch (error) {
+                console.log(`I could not keep the house`)
             }
 
         }, 2 * 60 * 60 * 1000);
